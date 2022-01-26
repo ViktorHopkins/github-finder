e9f82d2e4b35d0b2f6084417f9adb263e4ff6ce9
Viktor Hopkins <viktorhopkins00@gmail.com>
2022-01-26 22:11:29 +0100

    Creating the UserSearch component

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,12 +8,15 @@ const GITHUB_URL = "https://api.github.com/users"
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
-        loading: true
+        loading: false
     }
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
+    // Get initial users (testing purposes)
     const fetchUsers = async () => {
+        setLoading()
+
         const response = await fetch(GITHUB_URL)
 
         const data = await response.json()
@@ -23,6 +26,9 @@ export const GithubProvider = ({children}) => {
             payload: data
         })
     }
+
+    // Set loading 
+    const setLoading = () => dispatch({type: 'SET_LOADING'})
 
     return <GithubContext.Provider value={{
         users: state.users,
