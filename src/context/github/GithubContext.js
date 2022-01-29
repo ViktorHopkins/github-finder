0caa7afaa1b9419466245125203a1ae908e0d69f
Viktor Hopkins <viktorhopkins00@gmail.com>
2022-01-29 20:59:07 +0100

    Added Axios and cleaned up actions

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,90 +1,28 @@
-import { createContext, useReducer } from 'react';
-import githubReducer from './GithubReducer';
+import { createContext, useReducer } from 'react'
+import githubReducer from './GithubReducer'
 
 const GithubContext = createContext()
 
-const GITHUB_URL = "https://api.github.com"
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  }
 
-export const GithubProvider = ({children}) => {
-    const initialState = {
-        users: [],
-        user: {},
-        loading: false
-    }
+  const [state, dispatch] = useReducer(githubReducer, initialState)
 
-    const [state, dispatch] = useReducer(githubReducer, initialState)
-
-    // Get search results
-    const searchUsers = async (text) => {
-        setLoading()
-
-        const params = new URLSearchParams({
-            q: text,
-        })
-
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`)
-
-        const {items} = await response.json()
-        
-        dispatch({
-            type: 'GET_USERS',
-            payload: items
-        })
-    }
-
-    // Get a single user
-    const getUser = async (login) => {
-        setLoading()
-
-        const response = await fetch(`${GITHUB_URL}/users/${login}`)
-
-        if(response.status === 404){
-            window.location = '/notfound'
-        } else {
-            const data = await response.json()
-        
-            dispatch({
-                type: 'GET_USER',
-                payload: data
-            })
-        }
-    }
-
-    // Get user repos
-    const getUserRepos = async (login) => {
-        setLoading()
-
-        const params = new URLSearchParams({
-            sort: 'created',
-            per_page: 10
-        })
-
-        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`)
-
-        const data = await response.json()
-        
-        dispatch({
-            type: 'GET_REPOS',
-            payload: data
-        })
-    }
-
-    const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
-
-    // Set loading 
-    const setLoading = () => dispatch({type: 'SET_LOADING'})
-
-    return <GithubContext.Provider value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        searchUsers,
-        clearUsers,
-        getUser,
-        getUserRepos
-    }}>
-        { children }
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
     </GithubContext.Provider>
+  )
 }
 
 export default GithubContext
